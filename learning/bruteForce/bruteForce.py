d0e2563ee4169d9646cbd50025fcf3869a9b26a6
Dip Ghosh <dipghosraj@gmail.com>
2022-03-24 20:21:12 +0530

    brute force use case and structure

---
+++ learning/bruteForce/bruteForce.py
@@ -0,0 +1,9 @@
+import numpy as np
+from common.error.error import find_error
+
+
+def bruteForce():
+    '''
+    '''
+    print("brute force algo")
+    pass
---
+++ usecases/brute_force_test.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('E:/deep_learning_algo_version_1')
+
+from learning.bruteForce.bruteForce import bruteForce
+
+bruteForce()
